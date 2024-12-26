c83c5041014dd9f036796248c8fd851a1348d197
Palina Skakun <palinaskakun@gmail.com>
2024-12-26 12:16:03 +0100

    Initial Commit

---
+++ Wordle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wordle
+//
+//  Created by Palina Skakun on 12/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WordleTests/WordleTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordleTests.swift
+//  WordleTests
+//
+//  Created by Palina Skakun on 12/26/24.
+//
+
+import Testing
+@testable import Wordle
+
+struct WordleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
